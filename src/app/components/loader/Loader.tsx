18d1f5d973ff94c83a4f09cd153af0ad1c83741e
ali <alihgad2@gmail.com>
2024-07-12 02:42:40 +0300

    loader

--- src/app/components/loader/Loader.tsx
+++ src/app/components/loader/Loader.tsx
@@ -3,7 +3,7 @@ import styles from './styel.module.css'
 
 export default function Loader() {
   return (
-    <div className='flex justify-center items-center py-10'>
+    <div className='flex justify-center items-center py-10 h-screen'>
         <span className={styles.loader}></span>
     </div>
   )
